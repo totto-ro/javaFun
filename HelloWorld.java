6913e0cef2d2ee2aab8f05d74dc4e487abd6d1d7
Sofía Esquivel <sofia.esquivel@lcieducation.net>
2021-10-18 00:24:32 -0600

    Changes

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("My Name is Sofía");
-        System.out.println("I am 130 years old");
+        System.out.println("I am 127 years old");
         System.out.println("My hometown is San José, CR");
     }
 }
